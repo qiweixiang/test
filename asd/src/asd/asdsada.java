04914d40f3a2439e6cca0603cc56bec25442452c
qiweixiang <qiweixiang@qiweixiang>
2018-06-25 22:33:43 +0800

    ce

--- asd/src/asd/asdsada.java
+++ asd/src/asd/asdsada.java
@@ -3,6 +3,7 @@ package asd;
 public class asdsada {
 public static void main(String[] args) {
 	System.out.println();
-	System.out.println("asdas");
+	System.out.println("asdasmasaasal");
+	System.out.println();
 }
 }
